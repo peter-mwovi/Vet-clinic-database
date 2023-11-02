5fe43c345b72e970b3ffe816b6a83b521fb30c39
peter-mwovi <petermmwovi@gmail.com>
2023-11-02 18:32:26 +0300

    Edit Schema.sql file

--- schema.sql
+++ schema.sql
@@ -1,10 +1,16 @@
 /* Database schema to keep the structure of entire database. */
 
+/* FEATURE BRANCH 01-create-animals-table */
+
 CREATE TABLE animals (
     id int,
     name varchar(100),
     date_of_birth date,
     escape_attempts int,
     neutered boolean,
-    weight_kg float
+    weight_kg float,
 );
+
+/* FEATURE BRANCH 02-query-and-update-animals-table */
+
+ALTER TABLE animals ADD COLUMN species varchar(100);
